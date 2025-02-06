f221ce254a1896c85466eb716906325c43d038a8
ValentinoRicciMahawi <mahawivalentinoricci@gmail.com>
2025-02-06 14:37:52 +0700

    push tugas ulang lab 1

--- Tugas1-Ricci.py
+++ Tugas1-Ricci.py
@@ -1,1 +1,18 @@
-print ("Hello World")
+secret_word = "logic"
+letter = ["_"] * 5
+
+print ("Selamat adatang di permainan Hangman")
+print (f"Kata tersebut mengandung {len(secret_word)} huruf")
+
+while "_" in letter:
+    guess = input("Input a letter: ")
+    if guess in secret_word:
+        print ("Betul!")
+        for i in range(5):
+            if secret_word[i] == guess:
+                letter[i] = guess
+    else:
+        print ("Salah!")
+    print (" ".join(letter))
+    if "_" not in letter:
+        print("Selamat!")
